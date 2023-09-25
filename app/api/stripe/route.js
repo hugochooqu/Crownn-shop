1765b1f88598e94aea853d278d00476e654bf27b
Ugochukwu Victor <hugotchee1@gmail.com>
2023-09-25 20:20:01 +0100

    implementing stripe

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -6,47 +6,47 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
 export async function handler(req, res) {
   if (req.method === 'POST') {
     console.log(req)
-//     console.log('true')
-//     try {
-//         const params = {
-//             submit_type: 'pay',
-//             mode: 'payment',
-//             payment_method_types: ['card'],
-//             billing_address_collection: 'auto',
-//             shipping_options: [
-//                 {shipping_rate: 'shr_1NuGZAGneUBojUFKnuBWX01D'},
-//                 {shipping_rate: 'shr_1NuGbDGneUBojUFKnyLcjXFn'},
-//             ],
-//             line_items: req.body.cartItems.map((item) => {
-//                 const img = 'https://cdn.sanity.io/images/${projectId}/${dataset}/${item.asset._ref.replace("image-", "").replace("-webp", ".webp")}';
+    console.log('true')
+    try {
+        const params = {
+            submit_type: 'pay',
+            mode: 'payment',
+            payment_method_types: ['card'],
+            billing_address_collection: 'auto',
+            shipping_options: [
+                {shipping_rate: 'shr_1NuGZAGneUBojUFKnuBWX01D'},
+                {shipping_rate: 'shr_1NuGbDGneUBojUFKnyLcjXFn'},
+            ],
+            line_items: req.body.cartItems.map((item) => {
+                const img = 'https://cdn.sanity.io/images/${projectId}/${dataset}/${item.asset._ref.replace("image-", "").replace("-webp", ".webp")}';
 
-//                 return {
-//                     price_data: {
-//                         currency: 'usd',
-//                         product_data: {
-//                             name: item.name,
-//                             images: [img]
-//                         },
-//                         unit_amount: item.price * 100,
-//                     },
-//                     adjustable_quantity: {
-//                         enabled: true,
-//                         minimum: 1,
-//                     },  
-//                     quantity: item.quantity
-//                 }
-//             }),
-//             success_url: `${req.headers.origin}/?success=true`,
-//             cancel_url: `${req.headers.origin}/?canceled=true`,
-//           }
-//       // Create Checkout Sessions from body params.
-//       const session = await stripe.checkout.sessions.create(params);
-//       res.status(200).json(session);
-//     } catch (err) {
-//       res.status(err.statusCode || 500).json(err.message);
-//     }
-//   } else {
-//     res.setHeader('Allow', 'POST');
-//     res.status(405).end('Method Not Allowed');
+                return {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: item.name,
+                            images: [img]
+                        },
+                        unit_amount: item.price * 100,
+                    },
+                    adjustable_quantity: {
+                        enabled: true,
+                        minimum: 1,
+                    },  
+                    quantity: item.quantity
+                }
+            }),
+            success_url: `${req.headers.origin}/?success=true`,
+            cancel_url: `${req.headers.origin}/?canceled=true`,
+          }
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
 }
